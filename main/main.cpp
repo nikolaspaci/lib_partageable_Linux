d39adcb6a42f52358175249edc2cbed010b57010
nikolaspaci <nikolaspaci>
2021-03-04 19:44:19 +0100

    Utilisation de dlopen et dlsym (premier jet)

--- main/main.cpp
+++ main/main.cpp
@@ -1,20 +1,54 @@
 
 #include <iostream>
-#include "Composant1.h"
-#include "Composant2.h"
+#include <dlfcn.h>
 
 int main(int argc, char ** argv)
 {
+	char *error;
+	void *libComposant1;
+	void *libComposant2;
+	int (*composant1)(int,int);
+	int (*composant2)(int,int);
+	char (*getComposant1Version);
+	
+	libComposant1 = dlopen ("../lib/libComposant1.so",  RTLD_LOCAL|RTLD_LAZY);
+	if (!libComposant1) {
+		fputs (dlerror(), stderr);
+        exit(1);
+    }
+	
+	libComposant2 = dlopen ("../lib/libComposant2.so", RTLD_LOCAL|RTLD_LAZY);
+	if (!libComposant2) {
+		fputs (dlerror(), stderr);
+		exit(1);
+    }
+	
 	int data1=3;
 	int data2=5;
 
 	int valeur1;
 	int valeur2;
 
+	composant1 = (int (*)(int, int)) dlsym(libComposant1, "_Z10composant1ii");
+	if ((error = dlerror()) != NULL)  {
+		fputs(error, stderr);
+		exit(1);
+    }
+	composant2 = (int (*)(int, int)) dlsym(libComposant2, "_Z10composant2ii");
+	if ((error = dlerror()) != NULL)  {
+		fputs(error, stderr);
+		exit(1);
+    }
+	
 	valeur1=composant1(data1,data2);
-
 	valeur2=composant2(data1,data2);
 
-	std::cout << getComposant1Version() << std::endl;
+	getComposant1Version=(char (*)) dlsym(libComposant1, "_Z20getComposant1Versionv");
+	if ((error = dlerror()) != NULL)  {
+		fputs(error, stderr);
+		exit(1);
+    }
+	
+	std::cout << getComposant1Version << std::endl;
 	std::cout << "valeur 1 :" << valeur1 << " valeur 2 :" << valeur2 << std::endl;
 }
